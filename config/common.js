a67b755d2d433cb4dc822c3e275b98b5d90598ed
BG499022 <jjqian@best-inc.com>
2022-02-23 15:13:51 +0800

    feat: 打包第三方样式到组件中

--- config/common.js
+++ config/common.js
@@ -6,6 +6,7 @@ import postcss from 'rollup-plugin-postcss';
 import ts from 'rollup-plugin-typescript2';
 import jsx from 'acorn-jsx';
 import json from '@rollup/plugin-json';
+import clear from 'rollup-plugin-clear';
 
 const fileExtensions = ['.js', '.jsx', '.tsx'];
 const cssExtensions = ['.css', '.less'];
@@ -20,17 +21,22 @@ export default {
     input: './components/index.tsx',
     external: ['react', 'antd', 'react-dom'],
     plugins: [
+        clear({
+            targets: ['lib/']
+        }),
         json(),
         image(),
         postcss({
-            extract: true,
-            // Use named exports alongside default export.
-            namedExports: true,
             // Minimize CSS, boolean or options for cssnano.
             minimize: true,
             // Enable sourceMap.
             sourceMap: true,
-            extensions: cssExtensions
+            extensions: cssExtensions,
+            use: [
+                ['less', {
+                  javascriptEnabled: true
+                }]
+            ],
         }),
         nodeResolve({
             extensions: fileExtensions
@@ -40,7 +46,17 @@ export default {
             presets: ["@babel/preset-react"],
             babelHelpers: "bundled",
             extensions: fileExtensions,
-            exclude: 'node_modules/**'
+            exclude: 'node_modules/**',
+            plugins: [
+                [
+                    "import",
+                    {
+                        "libraryName": "antd",
+                        "libraryDirectory": "es",
+                        "style": true
+                    }
+                ]
+            ]
         }),
         commonjs()
     ],
