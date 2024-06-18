86445c57e32c59efba5d94fe33449c646c8f3989
钱晶晶 <jjqian@800best-inc.com>
2024-06-18 19:36:09 +0800

    feat: 排序001

---
+++ 字符串/字符串数组排序.js
@@ -0,0 +1,115 @@
+/**
+ * AA001A001A01
+ * AM001A001A01
+ * BD001A001A01
+ * BH001A001A01
+ * SA001B001A01
+ * SA001B001A01
+ * SC001A001A01
+ * SQ001A001A01
+ * SO001A001A01
+ * SO002A001
+ * SO002A001A01
+ * SZ002A001A01
+ * ZO001A001A01
+ */
+
+const arr = [
+	'ZO001A001A01',
+	'SO001A001A01',
+	'SZ002A001A01',
+	'SA001B001A01',
+	'SA001A001A01',
+	'SC001A001A01',
+	'SQ001A001A01',
+	'AM001A001A01',
+	'AA001A001A01',
+	'BH001A001A01',
+	'BD001A001A01',
+	'SO002A001A01',
+	'SO002A001',
+];
+
+const getCharType = (char) => {
+	return isNaN(char) ? 'string' : 'number';
+};
+
+const getSortInfo = (str, startIndex = 0, maxLength) => {
+	// console.log(str, startIndex, maxLength);
+	let sortIndex = startIndex;
+	let char = str[sortIndex];
+	let charType = getCharType(char);
+	let firstCharType = charType;
+	let sortMark = '';
+	while (char && charType === firstCharType && sortIndex < maxLength) {
+		sortMark = sortMark += char;
+		sortIndex++;
+		char = str[sortIndex];
+		charType = getCharType(char);
+	}
+	return {
+		value: str,
+		sortMark,
+		sortIndex,
+		sortType: firstCharType,
+	};
+};
+
+const sortByChar = (arr, maxLength) => {
+	console.log('++++++++++++++++++++++++++++++++++');
+	console.log(arr);
+	if (!arr || !arr.length) {
+		return [];
+	}
+	const markList = [];
+	const markToArrList = {};
+	const sortedArr = arr
+		.map(({ value, sortIndex = 0 }) => {
+			return getSortInfo(value, sortIndex, maxLength);
+		})
+		.sort((a, b) => {
+			if (a.sortType == 'number' && b.sortType == 'number') {
+				return a.sortMark - b.sortMark;
+			} else {
+				return String(a.sortMark).localeCompare(String(b.sortMark));
+			}
+		});
+	sortedArr.forEach((item) => {
+		const { sortMark } = item;
+		if (sortMark) {
+			if (!markList.includes(sortMark)) {
+				markList.push(sortMark);
+			}
+			if (!markToArrList[sortMark]) {
+				markToArrList[sortMark] = [];
+			}
+			markToArrList[sortMark].push(item);
+		}
+	});
+	// console.log(markList);
+	if (!markList.length || !markList) {
+		return [];
+	}
+	return markList.reduce((pre, next) => {
+		return [...pre, ...sortByChar(markToArrList[next], maxLength)];
+	}, []);
+};
+
+const sortStrings = (arr) => {
+	if (!arr || !arr.length) return [];
+	let maxLength = 0;
+	const sortArr = [];
+	arr.forEach((char) => {
+		const len = char.length;
+		maxLength = maxLength >= len ? maxLength : len;
+		sortArr.push({
+			value: char,
+			sortIndex: 0,
+		});
+	});
+	return sortByChar(sortArr, maxLength);
+};
+
+sortStrings(arr).forEach((item) => {
+	console.log(item.value, item.sortMark, item.sortType, item.sortIndex);
+});
